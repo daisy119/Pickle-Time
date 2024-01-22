9728e9de62a5f8a265d79fc235e8e01b62cfdde2
Jia Wang <daisy-a.119@hotmail.com>
2024-01-21 20:22:47 -0500

    set up Court model

---
+++ models/court.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+  content: String,
+  commentId: {type: Schema.Types.ObjectId, ref: "Profile"},
+}, {
+  timestamps: true
+})
+
+const courtSchema = new Schema({
+  name: String,
+  location: String,
+  phoneNuber: String,
+  author: [commentSchema]
+}, {
+  timestamps: true
+})
+
+const Court = mongoose.model('Court', courtSchema)
+
+export {
+  Court
+}
