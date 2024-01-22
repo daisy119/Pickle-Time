17e06a781d5a968de5203613bb01232e94d51f70
Jia Wang <daisy-a.119@hotmail.com>
2024-01-21 20:02:04 -0500

    set up Match model

---
+++ models/Match.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const matchSchema = new Schema({
+  date: Date,
+  friends: String,
+  courtId: {type: Schema.Types.ObjectId, ref: "Court"},
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+}, {
+  timestamps: true
+})
+
+const Match = mongoose.model('Match', matchSchema)
+
+export {
+  Match
+}
