eec788ae677a64e78d00dcc9f3f39566120ea70a
Jia Wang <daisy-a.119@hotmail.com>
2024-01-22 14:22:49 -0500

    modify button css

--- routes/matches.js
+++ routes/matches.js
@@ -10,6 +10,8 @@ router.get('/', matchesCtrl.index)
 router.get('/new', matchesCtrl.new)
 //GET localhost:3000/matches/:matchId
 router.get('/:matchId', matchesCtrl.show)
+//GET localhost:3000/matches/:matchId/edit
+
 //POST localhost:3000/matches
 router.post('/', isLoggedIn, matchesCtrl.create)
 //PATCH localhost:3000/matches/:matchId/flipFav
