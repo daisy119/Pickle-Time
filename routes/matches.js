f26c426b30919b4e599b5ef1a51ba62cfd33971f
Jia Wang <daisy-a.119@hotmail.com>
2024-01-22 01:17:33 -0500

    change the css for the index page

--- routes/matches.js
+++ routes/matches.js
@@ -8,6 +8,8 @@ const router = Router()
 router.get('/', matchesCtrl.index)
 //Get localhost:3000/matches/new
 router.get('/new', matchesCtrl.new)
+//POST localhost:3000/matches/
+
 //POST localhost:3000/matches
 router.post('/', isLoggedIn, matchesCtrl.create)
 
