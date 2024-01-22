57914bd03fc5a208c4864373392c18dc004dcc99
Jia Wang <daisy-a.119@hotmail.com>
2024-01-21 19:03:33 -0500

    modify Profile model with a reference to Court

--- models/profile.js
+++ models/profile.js
@@ -6,6 +6,7 @@ const profileSchema = new Schema({
   name: String,
   avatar: String,
   FriendsName: String,
+  favCourts: { type: Schema.Types.ObjectId, ref: 'Court'},
 }, {
   timestamps: true
 })
