dac4990edfca9375fbf9c0f12ea74e4c0fcbd8b6
Jia Wang <daisy-a.119@hotmail.com>
2024-01-22 23:06:35 -0500

    add courts router and controller

---
+++ routes/courts.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import * as courtsCtrl from '../controllers/courts.js'
+
+const router = Router()
+
+
+
+export {
+  router
+}
--- server.js
+++ server.js
@@ -22,6 +22,7 @@ import'./config/passport.js'
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as matchesRouter } from './routes/matches.js'
+import { router as courtsRouter } from './routes/courts.js'
 
 // create the express app
 const app = express()
@@ -63,6 +64,7 @@ app.use(passDataToView)
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
 app.use('/matches', matchesRouter)
+app.use('/courts', courtsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
